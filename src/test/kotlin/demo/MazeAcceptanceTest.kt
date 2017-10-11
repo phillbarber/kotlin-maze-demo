880aaddae42136f7a356fc7aead09148c1e9cb58
pergola <phillbarber@github.com>
2017-10-11 11:56:56 +0100

    Make acceptance test compile

--- src/test/kotlin/demo/MazeAcceptanceTest.kt
+++ src/test/kotlin/demo/MazeAcceptanceTest.kt
@@ -2,22 +2,18 @@ package demo
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Ignore
 import kotlin.test.assertEquals
 import org.junit.Test
 
 class MazeAcceptanceTest {
-    @Test fun simpleSolvableMaze() {
+    @Test @Ignore fun simpleSolvableMaze() {
         val solvedMaze: SolvedMaze = Maze("simple-solvable-maze.txt").solve()
 
         val route: List<Cell> = solvedMaze.getRoute()
-
         assertThat(route[0].type, `is`(Type.Start))
-
-
-        route.g
-        //assertThat(route[route.l].type, `is`(Type.Start))
+        assertThat(route[route.size].type, `is`(Type.Finish))
         solvedMaze.printSolution()
-
     }
 
 
